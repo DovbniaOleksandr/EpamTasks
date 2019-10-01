adbacb33b3dcd29a5febe742fe7b8d27a6dd2ab9
Dovbnia Oleksandr <sashadovbnya1@gmail.com>
2019-10-01 15:28:25 +0300

    Добавьте файлы проекта.

---
+++ EpamTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EpamTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
